52f7326367c1767ec46994269bf52deee9807c68
himanshuk <himanshuk@himanshuk-w7.cybage.com>
2015-04-29 12:51:10 +0530

    second push

--- DocumentGenerationService/src/main/java/com/dt/tms/TestClass.java
+++ DocumentGenerationService/src/main/java/com/dt/tms/TestClass.java
@@ -1,13 +1,22 @@
 package com.dt.tms;
 
-public class TestClass {
+//import org.apache.log4j.Logger;
+import org.junit.Test;
 
+public class TestClass {
+	//private static Logger logger = Logger.getLogger(TestClass.class);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+   //logger.error("error");
+   System.out.println("error");
+	}
+	
+	@Test
+	public void add(){
+		System.out.println("sdfsdfsdfsdf");
 	}
 
 }
